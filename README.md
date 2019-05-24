2a25278f2b9c88cfe4196c87ffd26c1254e8ab1d
vagrant <vagrant@vagrant.vm>
2019-05-24 08:12:38 +0000

    cssフォルダの追加とindex.phpの編集

--- README.md
+++ README.md
@@ -9,11 +9,14 @@ $ ip -a
 phpの簡易サーバーを起動
 $ php -S xxx.xxx.xxx.xxx:8000
 PHP 7.3.5-1+ubuntu18.04.1+deb.sury.org+1 Development Server started at Fri May 24 04:01:37 2019
-Listening on http://10.0.2.15:8000
+Listening on http://xxx.xxx.xxx.xxx:8000
 Document root is /home/vagrant/share/time_checker/time_checker
 Press Ctrl-C to quit.
 
 3行目のURLをコピーして、ブラウザに貼ると表示される
+
+phpを起動しなくても
+拡張子を.htmlに変えるとブラウザで開ける
 
 ※
 KIT-IA
--- time_checker/index.php
+++ time_checker/index.php
@@ -1,12 +1,44 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <title>Time Checker</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
+
 <body>
-    <h1>HOME</h1>
-    <h2>勤怠管理システム</h2>
-    <button><a href="./rogin.php">ログイン</a></button>
+    <div class="main">
+        <header>
+            <div class="container">
+                <h1 class="header-left">HOME</h1>
+                <h2 class="header-left">~田中・林研　勤怠管理システム~</h2>
+                <a class="header-right" href="./rogin.php">ログイン</a>
+            </div>
+        </header>
+        <div class="db-table">
+            <div class="container">
+                <h3>現在の状態</h3>
+                <table class="state">
+                    <tr>
+                        <th>名前</th>
+                        <th>学内</th>
+                        <th>帰宅</th>
+                    </tr>
+                    <tr>
+                        <td>foo</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>bar</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
+
 </html>
