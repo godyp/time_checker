db69572491d0dd1d02fbc0ba12d99bd81cdbcf54
vagrant <vagrant@vagrant.vm>
2019-05-24 09:40:00 +0000

    edited login page

--- time_checker/rogin.php
+++ time_checker/rogin.php
@@ -6,6 +6,17 @@
 </head>
 <body>
     <h1>ログイン</h1>
-    <button><a href="./edit.php">ログイン</a></button>
+    <form action="edit.php" method="post" autocomplete="off">
+    <table border="1">
+    <tr>
+    <td>学籍番号</td>
+      <td><input type="text" name="name" maxlength="8"></td>
+      <td colspan="2" align="center">
+         <input type="submit" value="ログイン">
+      </td>
+    </tr>
+  </table>
+  </form>
+    <!-- <button><a href="./edit.php">ログイン</a></button> -->
 </body>
 </html>
