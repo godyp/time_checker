042dc250f5aa00b123877ed496a3b6ae89838ea8
vagrant <vagrant@vagrant.vm>
2019-05-25 15:02:08 +0000

    index.phpの編集：データベースから読み取った値を表示できるようにした

--- server/index.php
+++ server/index.php
@@ -1,3 +1,17 @@
+<?php
+// 変数の初期化
+$db = null;
+$sql = null;
+$res = null;
+$row = null;
+
+$db = new SQLite3("./db/data.db");
+// データの取得
+$sql = "SELECT name,sid FROM members";
+$res = $db->query($sql);
+?>
+
+
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -19,22 +33,29 @@
         <div class="db-table">
             <div class="container">
                 <h3>現在の状態</h3>
-                <table class="state" border="1" border-color="#666" bgcolor="white" width="50%">
+                <table class="state">
                     <tr>
                         <th>名前</th>
-                        <th width="20%">学内</th>
-                        <th width="20%">帰宅</th>
+                        <th>学内</th>
+                        <th>帰宅</th>
                     </tr>
-                    <tr align="center">
-                        <td>foo</td>
-                        <td>◯</td>
-                        <td>-</td>
-                    </tr>
-                    <tr align="center">
-                        <td>bar</td>
-                        <td>-</td>
-                        <td>◯</td>
-                    </tr>
+                    <?php
+                    while ($row = $res->fetchArray()) {
+                        $sql = "SELECT sid FROM status WHERE sid=" . $row[1];
+                        $res_sts = $db->query($sql);
+                        $row_sts = $res_sts->fetchArray();
+                        echo '<tr>';
+                        echo '<td>' . $row[0] . '</td>';
+                        if ($row_sts[0] == NULL) {
+                            echo '<td>◯</td>';
+                            echo '<td>-</td>';
+                        } else {
+                            echo '<td>-</td>';
+                            echo '<td>◯</td>';
+                        }
+                        echo '</tr>';
+                    }
+                    ?>
                 </table>
             </div>
         </div>
