08290e40fb893ab08a460da0bd381521d6c91c9b
kosiro yoshimoto <cody7ky@gmail.com>
2019-05-27 16:49:08 +0900

    login.phpにDBと通信して、ログイン処理を追加 edit.phpに履歴表示機能を追加

--- server/pages/edit.php
+++ server/pages/edit.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 try{
-    $pdo = new PDO('sqlite:./../db/sample01.db');
+    $pdo = new PDO('sqlite:./../db/data.db');
     $pdo->exec('CREATE TABLE IF NOT EXISTS member(id INTEGER PRIMARY KEY, name TEXT, status INT)');
     //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
@@ -11,11 +11,11 @@ try{
 
 $id = $_SESSION['id'];
 $id = intval($id);
-$stmt = $pdo->prepare("SELECT * FROM member WHERE id = ?");
+$stmt = $pdo->prepare("SELECT * FROM members WHERE sid = ?");
 $stmt->bindValue(1, $id);
 $stmt->execute();
 $data = $stmt->fetch();
-$pdo = NULL; //DB接続解除
+//$pdo = NULL; //DB接続解除
 ?>
 
 <!DOCTYPE html>
@@ -32,11 +32,44 @@ $pdo = NULL; //DB接続解除
         <header>
             <div class="container">
                 <h2 class="header-left">
-                "<?php echo $data['name'] ?>さんの勤怠履歴"
+                    <?php echo $data['name'] ?>さんの勤怠履歴
                 </h2>
             </div>
         </header>
     </div>
+
+    <div class="db-table">
+            <div class="container">
+                <h3>履歴一覧</h3>
+                <table class="state">
+                    <tr>
+                        <th>名前</th>
+                        <th>入室時刻</th>
+                        <th>退室時刻</th>
+                    </tr>
+                    <?php
+                    $stmt = $pdo->prepare("SELECT * FROM history WHERE sid = ?");
+                    $stmt->bindValue(1, $id);
+                    $stmt->execute();
+                    $data = $stmt->fetchAll();
+
+                    $stmt = $pdo->prepare("SELECT COUNT(*) FROM history WHERE sid = ?");
+                    $stmt->bindValue(1, $id);
+                    $stmt->execute();
+                    $count =  $stmt->fetchColumn();
+
+                    for ($i=0; $i<$count; $i++) {
+                        echo '<tr>';
+                        echo '<td>' . $data[$i]['name']. '</td>';
+                        echo '<td>' . $data[$i]['in_time'] . '</td>';
+                        echo '<td>' . $data[$i]['out_time'] . '</td>';
+                        echo '</tr>';
+                        $i += 1;
+                    }
+                    ?>
+                </table>
+            </div>
+        </div>
 
     <button><a href="../index.php">GO HOME</a></button>
 </body>
--- server/pages/login.php
+++ server/pages/login.php
@@ -1,3 +1,34 @@
+<?php
+session_start();
+try{
+  $pdo = new PDO('sqlite:./../db/data.db');
+  $pdo->exec('CREATE TABLE IF NOT EXISTS member(id INTEGER PRIMARY KEY, name TEXT, status INT)');
+  //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+  }catch(PDOException $e){
+      exit('Connection failed:'.$e->getMessage());
+    }
+
+if(isset($_POST['datapost'])){
+  $_SESSION['id'] = $_POST['id'];
+  $id = $_SESSION['id'];
+  $id = intval($id);
+  echo $id;
+  //$stmt = $pdo->prepare("SELECT * FROM member WHERE id = ?");
+  $stmt = $pdo->prepare("SELECT COUNT(*) FROM members WHERE sid = ?");
+  $stmt->bindValue(1, $id);
+  $stmt->execute();
+  $count =  $stmt->fetchColumn();
+  $data = $stmt->fetch();
+  if($count == 0){
+    header('Location:error.php');
+  }else{
+    header('Location:edit.php');
+  }
+}
+$pdo = NULL; //DB接続解除
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
   <head>
@@ -13,13 +44,13 @@
         <h1 class="header-left">ログイン</h1>
       </div>
     </header>
-  <form action="edit.php" method="post" autocomplete="off">
+  <form action="" method="post" autocomplete="off">
     <table border="1">
       <tr>
         <td>学籍番号</td>
-        <td><input type="text" name="name" maxlength="8"></td>
+        <td><input type="text" name="id" maxlength="8"></td>
         <td colspan="2" align="center">
-          <input type="submit" value="ログイン">
+          <input type="submit" name= "datapost" value="ログイン">
         </td>
       </tr>
     </table>
