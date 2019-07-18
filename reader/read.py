0d5f7ec9851a9f868e561e686c86d307cfa03b2a
Godai Imazato <godai@sein.mse.kyutech.ac.jp>
2019-07-18 10:58:52 +0000

    ...

--- reader/read.py
+++ reader/read.py
@@ -172,7 +172,7 @@ def felica_waiting():
 
 # tableを表示する
 def select_table(table):
-    sql = 'select * from "' + str(table) + 'ORDER BY sid DESC"'
+    sql = 'select * from "' + str(table) + ' ORDER BY sid DESC"'
     for row in c.execute(sql):
         print(row)
 
